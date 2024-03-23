18ef40f8bd7b6fb3c4302d27cf2c5407c60720ab
julianliong <julianliong17@gmail.com>
2024-03-22 17:33:02 -0700

    added score feature

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ let questions = [
 
     {
         statement:"What is 5x9x8x7x6x76x872x728",
-        answers:["729479761920","74284328","93098137","x","a number"],
-        correctanswers:[0,3,4]
+        answers:["729479761930","74284328","93098137","x","2882387"],
+        correctanswers:3
     },
 
     {
@@ -36,6 +36,38 @@ let questions = [
         correctanswers:2
     }
 ]
+let score = 0
+function deletequestion(){
+    let questions = document.querySelector(".text")
+    let answers = document.querySelector(".answers")
+    
+    if(answers !== null || questions !== null){
+        answers.remove()
+        questions.remove()
+    }
+}
+
+let questionnumber = 0
+function nextquestion(){
+    let selectedanswer = document.querySelector("input[type='radio']:checked")
+    if (selectedanswer === null){
+        alert("Please select an answer before continuing.")
+        return
+    }
+    if (selectedanswer.hasAttribute("correct")){
+        score ++
+    }
+    deletequestion()
+    questionnumber += 1
+    if (questionnumber >= questions.length){
+        let scoretext = document.querySelector(".score")
+        scoretext.classList.remove("score")
+        scoretext.innerText = `Your score is ${score/questionnumber*100}%`
+    }
+    else {
+        createquestion(questionnumber)
+    }
+}
 
 function createquestion(questionnum){
     let singlequestion = questions[questionnum]
@@ -51,11 +83,15 @@ function createquestion(questionnum){
         let input = document.createElement("input")
         label.innerText = answer
         input.type = "radio"
-
+        input.name = statement.innerText
+        
+        if (singlequestion.correctanswers == i){
+            input.setAttribute("correct",true)
+        }
         answers.appendChild(input)
         answers.appendChild(label)
     }
     document.querySelector("body").appendChild(tempcontent)
 }
 
-createquestion(4)
+createquestion(questionnumber)
